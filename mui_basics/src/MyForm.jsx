1d11d884c6a47f62716e8e69134058596f45cee1
m-ahmadzafar <161350207+m-ahmadzafar@users.noreply.github.com>
2024-07-15 09:34:57 +0500

    Setup json-server and used radio buttons.

--- mui_basics/src/MyForm.jsx
+++ mui_basics/src/MyForm.jsx
@@ -1,57 +1,65 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
 
 const MyForm = () => {
-    return (<>
-
-    <Typography
-    variant='h2'
-    >
+  return (
+    <>
+      <Typography variant='h2'>
         MUI FORM
-    </Typography>
-    <form>
-    <TextField
-    label="Name" 
-    variant='outlined'
-    required
-    sx={{
-        marginRight: 5 
-    }}/>
+      </Typography>
+      <form>
+        <TextField
+          label="Name" 
+          variant='outlined'
+          required
+          sx={{ marginRight: 5 }}
+        />
 
-    <TextField
-    label="Fathers Name"
-    variant='filled'
-    sx={{
-        marginRight: 5 
-    }}/>
+        <TextField
+          label="Fathers Name"
+          variant='filled'
+          sx={{ marginRight: 5 }}
+        />
 
-    <TextField
-    label="Mother's Name"
-    variant='standard'
-    sx={{
-        marginRight: 5 
-    }}/>
-    <TextField
-    label="Age"
-    type='number'
-    variant='standard'
-    sx={{
-        marginRight: 5 
-    }}/>
+        <TextField
+          label="Mother's Name"
+          variant='standard'
+          sx={{ marginRight: 5 }}
+        />
+        
+        <TextField
+          label="Age"
+          type='number'
+          variant='standard'
+          sx={{ marginRight: 5 }}
+        />
 
-    <br />
-    <Button
-    type='submit'
-    variant='contained'
-    sx={{
-        marginTop: 5
-    }}>
-    Submit
-    </Button>
-    
-    </form>   
-    </>  );
+        <br />
+
+        <FormControl>
+          <RadioGroup>
+            <FormControlLabel control={<Radio />} label="work" value="work" />
+            <FormControlLabel control={<Radio />} label="todos" value="todos" />
+            <FormControlLabel control={<Radio />} label="money" value="money" />
+            <FormControlLabel control={<Radio />} label="reminders" value="reminders" />
+          </RadioGroup>
+        </FormControl>
+        
+        <Button
+          type='submit'
+          variant='contained'
+          sx={{ marginTop: 5 }}
+        >
+          Submit
+        </Button>
+      </form>   
+    </>
+  );
 }
- 
+
 export default MyForm;
--- mui_basics/src/Notes.jsx
+++ mui_basics/src/Notes.jsx
@@ -1,40 +1,53 @@
 import Typography from '@mui/material/Typography';
-import HeroBlock from './HeroBlock';
-import {Link} from 'react-router-dom';
-
+// import HeroBlock from './HeroBlock';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Notes = () => {
-    return (<div>
-        {/* <HeroBlock/> */}
-       <p>notes page</p>
-       <Typography
+  const [notes, setNotes] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:8000/notes")
+      .then(res => res.json()) // Added parentheses to json() method
+      .then(data => setNotes(data));
+  }, []);
+  
+  return (
+    <div>
+      {/* <HeroBlock/>
+      <p>notes page</p>
+      <Typography
         variant="h1"
         component="h2"
         color="primary" >
-  h1. Heading
-</Typography>
-<Typography
+        h1. Heading
+      </Typography>
+      <Typography
         variant="h1"
         component="p"
         color="secondary" >
-  h2 Heading 
-</Typography>
+        h2 Heading 
+      </Typography> */}
 
-<Typography variant="h5"
-sx={{
-  bgcolor: 'pink',
-  textAlign: 'center',
-  width: 150
-}}>
-  <p>Page Links</p>
-  <Link to="/button">button </Link> <br />
-  <Link to="/custom">custom </Link> <br />
-  <Link to="/card">card </Link> <br />
-  <Link to="/create">create </Link> <br />
-  <Link to="/form">form</Link> <br />
-</Typography>
- 
-    </div>  );
+      {notes.map(note => (
+        <p key={note.id}>{note.title}</p>
+      ))}
+
+      <Typography variant="h5"
+        sx={{
+          bgcolor: 'pink',
+          textAlign: 'center',
+          width: 150
+        }}>
+        <p>Page Links</p>
+        <Link to="/button">button</Link> <br />
+        <Link to="/custom">custom</Link> <br />
+        <Link to="/card">card</Link> <br />
+        <Link to="/create">create</Link> <br />
+        <Link to="/form">form</Link> <br />
+      </Typography>
+    </div>
+  );
 }
- 
+
 export default Notes;
